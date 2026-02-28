c8d5697bfe7890c350776a5ee80dd47468a8b38e
Ashish T <ashishh459@gmail.com>
2026-02-28 12:32:48 +0545

    fix

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://belanepal.com', // Replace with your actual domain
+  generateRobotsTxt: true, // (optional)
+  outDir: 'out', // output directory for static export
+  // ...other options
+};
+
+export default config;
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,1 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://belanepal.com', // Replace with your actual domain
-  generateRobotsTxt: true, // (optional)
-  outDir: 'out', // output directory for static export
-  // ...other options
-}
+
